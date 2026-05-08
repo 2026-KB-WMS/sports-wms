48933610eac15864ee6459092c62b15f1592f9b0
JaeYoon <hanjyoon01@gmail.com>
2026-05-08 09:27:51 +0900

    initial commit

---
+++ src/main/java/com/example/sportswms/SportsWmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportswms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsWmsApplication.class, args);
+    }
+
+}
