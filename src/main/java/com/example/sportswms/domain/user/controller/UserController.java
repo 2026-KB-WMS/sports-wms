f36dfa31f62b2c05c025d1ca8df7fb6aa699af48
JaeYoon <hanjyoon01@gmail.com>
2026-05-21 17:19:28 +0900

    feat: Controller, 로그인/회원가입 페이지 구현 (#2)

---
+++ src/main/java/com/example/sportswms/domain/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.sportswms.domain.user.controller;
+
+import com.example.sportswms.domain.user.dto.SignUpRequestDTO;
+import com.example.sportswms.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String loginPage() { return "login"; }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(SignUpRequestDTO dto) {
+        userService.signup(dto);
+        return "redirect:/";
+    }
+}
