a1feaf17c672030c8839ab0f3338c151164ad508
JaeYoon <hanjyoon01@gmail.com>
2026-05-21 17:40:23 +0900

    feat: SecurityConfig 구현, 비밀번호 암호화 (#2)

--- src/main/java/com/example/sportswms/domain/user/service/UserService.java
+++ src/main/java/com/example/sportswms/domain/user/service/UserService.java
@@ -1,17 +1,21 @@
 package com.example.sportswms.domain.user.service;
 
 import com.example.sportswms.domain.user.dto.SignUpRequestDTO;
+import com.example.sportswms.domain.user.entity.Role;
 import com.example.sportswms.domain.user.entity.User;
 import com.example.sportswms.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
+
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(SignUpRequestDTO dto) {
         String loginId = dto.getLoginId();
@@ -23,11 +27,12 @@ public class UserService implements UserDetailsService {
 
         User user = new User();
         user.setLoginId(loginId);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         user.setName(name);
         user.setEmail(email);
         user.setPhoneNum(phoneNum);
         user.setAddress(address);
+        user.setRole(Role.ROLE_GENERAL_MANAGER);
 
         userRepository.save(user);
     }
