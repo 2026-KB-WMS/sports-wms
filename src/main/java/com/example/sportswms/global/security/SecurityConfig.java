a345a4051f6ee73a06a84a70971b3a3a0868886a
JaeYoon <hanjyoon01@gmail.com>
2026-06-08 11:02:18 +0900

    refactor: 권한별 메뉴 접근 세분화 (#18) - 본사 관리자 : 상품, 창고, 재고, 지점 조회 - 창고 관리자 : 창고, 재고 조회 - 일반 회원 : 발주 등록, 지점 조회 - 추후 기능 세분화 예정

--- src/main/java/com/example/sportswms/global/security/SecurityConfig.java
+++ src/main/java/com/example/sportswms/global/security/SecurityConfig.java
@@ -32,26 +32,27 @@ public class SecurityConfig {
 
         // 로그아웃 설정
         http.logout((auth) -> auth
-                .logoutUrl("/logout")       // HTML에서 요청할 로그아웃 주소
-                .logoutSuccessUrl("/")      // 로그아웃 성공 후 메인 홈 화면으로 이동!
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
                 .permitAll()
         );
 
         // 권한별 인가 필터
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/signup").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/product").permitAll()
-                        .requestMatchers("/warehouse/**").permitAll()
-                        .requestMatchers("/inventory").permitAll()
-                        .requestMatchers("/store/**").permitAll()
-                        .requestMatchers("/order/**").permitAll()
-                        .anyRequest().denyAll()
+                        // 모든 회원 접근 가능
+                        .requestMatchers("/", "/signup", "/login").permitAll()
+                        // 본사 관리자 접근 가능
+                        .requestMatchers("/product/**", "/store/**").hasRole("GENERAL_MANAGER")
+                        // 본사 관리자 및 창고 관리자 접근 가능 (창고, 지점 관리)
+                        .requestMatchers("/warehouse/**").hasRole("WAREHOUSE_MANAGER")
+                        // 일반 회원 (점주) 접근 가능 (발주)
+                        .requestMatchers("/order/**").hasRole("USER")
+                        // 재고 조회는 로그인한 회원 모두 접근 가능
+                        .requestMatchers("/inventory").hasAnyRole("GENERAL_MANAGER", "WAREHOUSE_MANAGER", "USER")
+                        // 그 외의 모든 요청은 로그인 필요
+                        .anyRequest().authenticated()
                 );
-
-        // 최종 빌드
         return http.build();
     }
 }
